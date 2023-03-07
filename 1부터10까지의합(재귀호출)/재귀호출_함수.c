6823a1ebe714ebb16e68435ab4e73f8cd7e32903
dla9944 <dla9944@naver.com>
2023-03-07 21:31:33 +0900

    함수

---
+++ 1부터10까지의합(재귀호출)/재귀호출_함수.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int rec_func(int b);
+
+int main(void)
+{
+	int b;
+	b = rec_func(10);
+	printf("°á°ú : %d\n", b);
+	return 0;
+}
+
+int rec_func(int b)
+{
+	if (b== 1) return 1;
+	else return (b + rec_func(b - 1));
+}
+
